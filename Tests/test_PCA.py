f085c04dabc88d21cdcb6be5fe753bdca4d167e0
Jacob Hamer <jacobhhamer@gmail.com>
2017-03-22 12:47:37 -0400

    Changed path

--- Tests/test_PCA.py
+++ Tests/test_PCA.py
@@ -1,27 +1,26 @@
-import PCA 
-import pytest
+from PCA.PCA import *
 
 def test_read_data1():
-	file='/Users/jacobhamer/Desktop/College\ Work/Spring\ 2017/PHYS767/Jacob-Practical-Stats-Exercises/4point3redone.dat'
+	file='data/4point3redone.dat'
 	tb=PCA.read_data(file)
 	assert tb.shape==(22,13)
 
 def test_normalize_data():
-	file='/Users/jacobhamer/Desktop/College\ Work/Spring\ 2017/PHYS767/Jacob-Practical-Stats-Exercises/4point3redone.dat'
+	file='data/4point3redone.dat'
 	tb=PCA.read_data(file)
 	normed=PCA.normalize_data(tb)
 	for c in normed.columns:
 		assert c.mean()==0
 
 def test_eigenpairs1():
-	file='/Users/jacobhamer/Desktop/College\ Work/Spring\ 2017/PHYS767/Jacob-Practical-Stats-Exercises/4point3redone.dat'
+	file='data/4point3redone.dat'
 	tb=PCA.read_data(file)
 	normed=PCA.normalize_data(tb)
 	pairs=PCA.eigenpairs(normed)
 	assert np.sum(pairs[:,0])==1
 
 def test_eigenpairs2():
-	file='/Users/jacobhamer/Desktop/College\ Work/Spring\ 2017/PHYS767/Jacob-Practical-Stats-Exercises/4point3redone.dat'
+	file='data/4point3redone.dat'
 	tb=PCA.read_data(file)
 	normed=PCA.normalize_data(tb)
 	pairs=PCA.eigenpairs(normed)
@@ -29,7 +28,7 @@ def test_eigenpairs2():
 		assert pairs[i+1][0] > pairs[i][0]
 
 def test_eigenpairs3():
-	file='/Users/jacobhamer/Desktop/College\ Work/Spring\ 2017/PHYS767/Jacob-Practical-Stats-Exercises/4point3redone.dat'
+	file='data/4point3redone.dat'
 	tb=PCA.read_data(file)
 	normed=PCA.normalize_data(tb)
 	pairs=PCA.eigenpairs(normed)
@@ -37,7 +36,7 @@ def test_eigenpairs3():
 		assert np.linalg.norm(pairs[i][1])==1
 
 def test_select_pairs1():
-	file='/Users/jacobhamer/Desktop/College\ Work/Spring\ 2017/PHYS767/Jacob-Practical-Stats-Exercises/4point3redone.dat'
+	file='data/4point3redone.dat'
 	tb=PCA.read_data(file)
 	normed=PCA.normalize_data(tb)
 	pairs=PCA.eigenpairs(normed)
@@ -45,7 +44,7 @@ def test_select_pairs1():
 	assert len(selected_pairs)==2
 
 def test_select_pairs2():
-	file='/Users/jacobhamer/Desktop/College\ Work/Spring\ 2017/PHYS767/Jacob-Practical-Stats-Exercises/4point3redone.dat'
+	file='data/4point3redone.dat'
 	tb=PCA.read_data(file)
 	normed=PCA.normalize_data(tb)
 	pairs=PCA.eigenpairs(normed)
@@ -53,7 +52,7 @@ def test_select_pairs2():
 	assert np.min(selected_pairs[:,0])>.2
 
 def test_transform_data():
-	file='/Users/jacobhamer/Desktop/College\ Work/Spring\ 2017/PHYS767/Jacob-Practical-Stats-Exercises/4point3redone.dat'
+	file='data/4point3redone.dat'
 	tb=PCA.read_data(file)
 	normed=PCA.normalize_data(tb)
 	pairs=PCA.eigenpairs(normed)
